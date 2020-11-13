435cf644b9d0d1edee12917558e1cecc7c4cd9bd
Daniel Backes <danielbackes@protonmail.com>
2020-11-13 18:30:11 -0400

    feat(react component): :sparkles: Create Dashboard's Header component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { HeaderTitle } from '@react-navigation/stack';
+import React, { useCallback } from 'react';
 
 import { useAuth } from '../../hooks/auth';
 
+import {
+  Container,
+  Header,
+  Title,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
+
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const { navigate } = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Sign Out" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <Title>
+          Bem vindo,
+          {'\n'}
+          <UserName>{user.name}</UserName>
+        </Title>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
+    </Container>
   );
 };
 
