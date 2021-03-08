c7cd333cfb1ab38e17d3a856b9a296c8b6a47e85
Daniel Backes <danielbackes@protonmail.com>
2021-03-07 21:02:44 -0400

    feature: Customize the tab menu items

--- src/navigations/app/home/index.tsx
+++ src/navigations/app/home/index.tsx
@@ -1,16 +1,48 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Feather';
 
 import HairdresserScreen from './hairdresser';
-// import AppointmentScreen from './appointment';
+import AppointmentScreen from '../../../screens/Appointment';
 
-const HomeBottomTabs = createBottomTabNavigator();
+const HomeBottomTab = createBottomTabNavigator();
+
+const bottomTabItemConfig = {
+  activeBackgroundColor: '#3e3b47',
+  activeTintColor: '#ff9000',
+  inactiveBackgroundColor: '#3e3b47',
+  inactiveTintColor: '#999591',
+  labelStyle: {
+    fontFamily: 'RobotoSlab-Regular',
+  },
+};
 
 const HomeRoutes: React.FC = () => (
-  <HomeBottomTabs.Navigator initialRouteName="Hairdresser">
-    <HomeBottomTabs.Screen name="Hairdresser" component={HairdresserScreen} />
-    {/* <HomeBottomTabs.Screen name="Appointment" component={AppointmentScreen} /> */}
-  </HomeBottomTabs.Navigator>
+  <HomeBottomTab.Navigator
+    initialRouteName="Hairdressers"
+    tabBarOptions={bottomTabItemConfig}
+  >
+    <HomeBottomTab.Screen
+      name="Hairdressers"
+      component={HairdresserScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => <Icon
+          size={size}
+          color={color}
+          name={'meh'} />
+      }}
+    />
+    <HomeBottomTab.Screen
+      name="Appointments"
+      component={AppointmentScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => <Icon
+          size={size}
+          color={color}
+          name={'layers'} />
+      }}
+    />
+  </HomeBottomTab.Navigator>
 );
 
 export default HomeRoutes;
