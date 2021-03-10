5c924cad6c740538a2b47ae8c9d9e402d2751f57
Daniel Backes <danielbackes@protonmail.com>
2021-03-09 22:09:28 -0400

    feat: Create the About screen

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -1,14 +1,92 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
+import {
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+
 import {
   Container,
+  Header,
+  BackButton,
+  Body,
+  HiThere,
   Title,
+  Description,
+  ButtonRow,
+  CodeButton,
+  WebsiteButton,
+  GooglePlayButton,
+  GithubButton,
+  StackOverflowButton,
+  LinkedinButton,
+  ProtonButton,
 } from './styles';
 
 const About: React.FC = () => {
+  const navigation = useNavigation();
+
+  const handleGoBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
   return (
-    <Container>
-      <Title>About</Title>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ flex: 1 }}
+      >
+        <Container>
+          <Header>
+            <BackButton onPress={handleGoBack}>
+              <FeatherIcon name="chevron-left" size={24} color="#999591" />
+            </BackButton>
+          </Header>
+          <Body>
+
+            <HiThere>Hi there, I'm Daniel Backes</HiThere>
+            <Description>
+              I devoloped this app during the Rocketseat GoStack course.
+            </Description>
+            <Description>
+              After that, I added some more features to finish them.
+            </Description>
+
+            <Title>See also:</Title>
+            <ButtonRow>
+              <CodeButton url="https://github.com/danielbackes/rocketseat-gobarber-mobile">
+                <FontAwesomeIcon name="git" size={24} color="#fff" />
+              </CodeButton>
+              <WebsiteButton url="https://gobarber.danielbackes.dev">
+                <FontAwesomeIcon name="globe" size={24} color="#fff" />
+              </WebsiteButton>
+              <GooglePlayButton url="https://play.google.com/store/apps/details?id=dev.danielbackes.gobarber">
+                <FontAwesomeIcon name="google" size={24} color="#fff" />
+              </GooglePlayButton>
+            </ButtonRow>
+
+            <Title>Connect with me:</Title>
+            <ButtonRow>
+              <GithubButton url="https://github.com/danielbackes">
+                <FontAwesomeIcon name="github" size={24} color="#fff" />
+              </GithubButton>
+              <StackOverflowButton url="https://stackoverflow.com/users/10463549/daniel-backes">
+                <FontAwesomeIcon name="stack-overflow" size={24} color="#fff" />
+              </StackOverflowButton>
+              <LinkedinButton url="https://www.linkedin.com/in/daniel-backes-73177224">
+                <FontAwesomeIcon name="linkedin" size={24} color="#fff" />
+              </LinkedinButton>
+              <ProtonButton url="mailto:danielbackes@protonmail.com">
+                <FontAwesomeIcon name="envelope" size={24} color="#fff" />
+              </ProtonButton>
+            </ButtonRow>
+          </Body>
+        </Container>
+      </KeyboardAvoidingView>
+    </>
   );
 };
 
--- src/screens/About/styles.ts
+++ src/screens/About/styles.ts
@@ -1,13 +1,85 @@
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import URLButton from '../../components/URLButton';
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
-  padding: 0 30px 40px;
+`;
+
+export const Header = styled.View`
+  padding: 15px;
+  padding-top: ${getStatusBarHeight() + 15}px;
+  background: #28262e;
+`;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const Body = styled.View`
+  flex: 1;
+  padding: 0 30px 0;
   background: #312e38;
 `;
 
+export const HiThere = styled.Text`
+  font-size: 15px;
+  color: #ff9000;
+  font-family: 'RobotoSlab-Medium';
+  margin: 35px 0 15px;
+`;
+
 export const Title = styled.Text`
-  color: #fff;
-  align-self: center;
+  font-size: 18px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 45px 0 20px;
+`;
+
+export const Description = styled.Text`
+  font-size: 13px;
+  color: #bab8b5;
+  font-family: 'RobotoSlab-Medium';
+  margin-top: 5px;
+`;
+
+export const ButtonRow = styled.View`
+  flex-direction: row;
+  justify-content: space-evenly;
+`;
+
+export const URLButtonBase = styled(URLButton)`
+  padding: 15px;
+  width: 60px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
+`;
+
+export const CodeButton = styled(URLButtonBase)`
+  background-color: #f05032;
+`;
+
+export const WebsiteButton = styled(URLButtonBase)`
+  background-color: #006d77;
+`;
+
+export const GooglePlayButton = styled(URLButtonBase)`
+  background-color: #414141;
+`;
+
+export const GithubButton = styled(URLButtonBase)`
+  background-color: #181717;
+`;
+
+export const StackOverflowButton = styled(URLButtonBase)`
+  background-color: #FE7A16;
+`;
+
+export const LinkedinButton = styled(URLButtonBase)`
+  background-color: #0A66C2;
+`;
+
+export const ProtonButton = styled(URLButtonBase)`
+  background-color: #8B89CC;
 `;
