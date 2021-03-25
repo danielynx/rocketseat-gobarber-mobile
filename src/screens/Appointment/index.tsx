b50f9f530544e6222144dac3eaf204185120a8c5
Daniel Backes <danielbackes@protonmail.com>
2021-03-24 21:07:08 -0400

    feat: Add refresh control on Appointment screen

--- src/screens/Appointment/index.tsx
+++ src/screens/Appointment/index.tsx
@@ -17,7 +17,7 @@ import {
   AppointmentMetaText,
   AppointmentsEmpty
 } from './styles';
-import { Alert } from 'react-native';
+import { Alert, RefreshControl } from 'react-native';
 
 export interface Appointment {
   id: string;
@@ -40,27 +40,49 @@ export interface FormatedAppointment {
 }
 
 const Appointment: React.FC = () => {
+  const [refreshing, setRefreshing] = useState(false);
   const [appointments, setAppointments] = useState<FormatedAppointment[]>([]);
 
+  const formatAppointments = (appointments: Appointment[]): FormatedAppointment[] => {
+    const now = new Date();
+
+    return appointments.map(({ id, date, provider }: Appointment) => {
+      const parsedDate = parseISO(date);
+
+      return {
+        id,
+        day: format(parsedDate, "eee', 'MMM' 'dd', 'yyyy"),
+        hour: format(parsedDate, "HH:ss' h'"),
+        past: isBefore(parsedDate, now),
+        provider,
+      };
+    });
+  }
+
   useEffect(() => {
-    api.get<Appointment[]>('appointments/user/me')
-      .then(response => {
-        const now = new Date();
+    (async () => {
+      const response = await api.get<Appointment[]>('appointments/user/me');
 
-        const formatedAppointments = response.data.map(({ id, date, provider }: Appointment) => {
-          const parsedDate = parseISO(date);
+      const formatedAppointments = formatAppointments(response.data);
 
-          return {
-            id,
-            day: format(parsedDate, "eee', 'MMM' 'dd', 'yyyy"),
-            hour: format(parsedDate, "HH:ss' h'"),
-            past: isBefore(parsedDate, now),
-            provider,
-          };
-        });
+      setAppointments(formatedAppointments);
+    })();
+  }, []);
 
-        setAppointments(formatedAppointments);
-      });
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+
+    (async () => {
+      const response = await api.get<Appointment[]>('appointments/user/me');
+
+      const formatedAppointments = formatAppointments(response.data);
+
+      setAppointments(formatedAppointments);
+
+      setRefreshing(false);
+    })();
+
   }, []);
 
   return (
@@ -71,6 +93,12 @@ const Appointment: React.FC = () => {
         data={appointments}
         keyExtractor={appointment => appointment.id}
         ListEmptyComponent={<AppointmentsEmpty>There isn't any appointment</AppointmentsEmpty>}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }
         renderItem={({ item: { id, day, hour, past, provider: { name, avatar_url } } }) => (
           <AppointmentContainer past={past}>
             {avatar_url
